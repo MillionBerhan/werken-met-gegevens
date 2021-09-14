c67c912a9453c03a964d0dfcc0cdcf53d2015160
MILLION BERHAN <99065113@mydavinci.com>
2021-09-14 10:03:23 +0200

    F1.2.01.L3

---
+++ F1.2.01.L3 - hallo.py
@@ -0,0 +1,7 @@
+
+crossaintjes = 0.39
+stokbroden = 2.78
+crossaintjesAantal= int(input('hoeveel crossaintjes wilt u?'))
+print (crossaintjesAantal*crossaintjes)
+StokbrodenAantal = int(input ('hoeveel stokbroden wilt u?'))
+print (StokbrodenAantal*stokbroden)
